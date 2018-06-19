3bd966531782b1391b2fa1d106ced25a8b042fa8
Lauren Richert <lsrichert@gmail.com>
2018-06-19 08:59:30 -0500

    get new master

--- src/scripts/taskList.js
+++ src/scripts/taskList.js
@@ -37,25 +37,3 @@ const taskList = Object.create (prototypeObject, {
   taskList.createTask("new pet", "get pet salamander", "2/2/2019", "to do");
   console.log("list array", listArray)
 
-
-const saveDatabase = function (databaseObject, localStorageKey) {
-    /*
-        Convert the Object into a string.
-    */
-    const stringifiedDatabase = JSON.stringify(databaseObject)
-
-    /*
-        Create a key in local storage, and store the string
-        version of your inventory database as the value
-    */
-   console.log("database object", databaseObject)
-    localStorage.setItem(localStorageKey, stringifiedDatabase)
-}
-
-saveDatabase(listArray, "ArrayOfTasks")
-
-
-
-// function writeTaskToDom = () => {
-//       for (let i = 0; i < )
-//   }
