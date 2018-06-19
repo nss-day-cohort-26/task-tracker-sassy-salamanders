f146487b35cfc02f1ef803bab73f6bbc20a54a22
leahgwin <leahgwin@gmail.com>
2018-06-19 12:02:29 -0500

    input form and task list object are connected

--- src/scripts/taskList.js
+++ src/scripts/taskList.js
@@ -1,44 +1,47 @@
 const databaseFunction = require("./main");
-const listArray = []
-prototypeObject = {
-    newTask: function () {
-        return this.name + summary+due+statusLocation
-    }
-}
-const taskList = Object.create (prototypeObject, {
+// prototypeObject = {
+//     newTask: function () {
+//         return this.name + summary+due+statusLocation
+//     }
+// }
+const taskList = Object.create({}, {
+    listArray: {
+        value: []
+    },
     createTask: {
-          value: function (taskName, description, dueDate, location) {
-              let newTask = Object.create(null, {
-                  name: {
-                      value: taskName,
-                      writable: true,
-                      enumerable: true,
-                  },
-                  summary: {
-                      value: description,
-                      writable: true,
-                      enumerable: true,
-                  },
-                  due: {
-                      value: dueDate,
-                      writable: true,
-                      enumerable: true,
-                  },
-                  statusLocation: {
-                      value: location,
-                      writable: true,
-                      enumerable: true,
-                  }
-              })
-            listArray.push(newTask);
-            }
-   }
-  })
-  taskList.createTask("get drunk", "coding stressing me out", "2/2/2018", "to do");
-  taskList.createTask("new pet", "get pet salamander", "2/2/2019", "to do");
-  console.log(listArray)
+        value: function (taskName, description, dueDate, location) {
+            let newTask = Object.create(null, {
+                name: {
+                    value: taskName,
+                    writable: true,
+                    enumerable: true,
+                },
+                summary: {
+                    value: description,
+                    writable: true,
+                    enumerable: true,
+                },
+                due: {
+                    value: dueDate,
+                    writable: true,
+                    enumerable: true,
+                },
+                statusLocation: {
+                    value: location,
+                    writable: true,
+                    enumerable: true,
+                }
+            })
+            this.listArray.push(newTask);
+        }
+    }
+})
+taskList.createTask("get happy", "coding stressing me out", "2/2/2018", "to do");
+taskList.createTask("new pet", "get pet salamander", "2/2/2019", "to do");
+console.log(taskList.listArray)
 
-  //console.log("list array", listArray)
- databaseFunction.saveDatabase(listArray, "ArrayOfTasks")
- console.log(databaseFunction.saveDatabase)
+//console.log("list array", listArray)
+databaseFunction.saveDatabase(taskList.listArray, "ArrayOfTasks")
+console.log(databaseFunction.saveDatabase)
 
+module.exports = taskList;
