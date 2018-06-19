7f56bd28f6df2a0837031fdf58379e9b602f31a0
Emily Venturino <ebventurino@gmail.com>
2018-06-19 09:52:45 -0500

    added submit form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,7 @@
 const array = require("taskList");
 const saveDatabase = function (databaseObject, localStorageKey) {
-​
   const stringifiedDatabase = JSON.stringify(databaseObject)
-​
   console.log("database object", databaseObject)
   localStorage.setItem(localStorageKey, stringifiedDatabase)
 }
-​
 saveDatabase(listArray, "ArrayOfTasks")
