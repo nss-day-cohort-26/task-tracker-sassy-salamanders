9537c35522270d77725bdaf70996890452618ee3
Adelaide Yoder <adelaide.yoder@gmail.com>
2018-06-19 11:14:13 -0500

    deleted some stuff

--- src/scripts/taskList.js
+++ src/scripts/taskList.js
@@ -42,34 +42,3 @@ const taskList = Object.create (prototypeObject, {
  databaseFunction.saveDatabase(listArray, "ArrayOfTasks")
  console.log(databaseFunction.saveDatabase)
 
-<<<<<<< HEAD
-//store the array list to Local Storage__________________________________________________________
-const saveDatabase = function (databaseObject, localStorageKey) {
-
-    const stringifiedDatabase = JSON.stringify(databaseObject)
-
-    console.log("stored to local storage", databaseObject)
-    localStorage.setItem(localStorageKey, stringifiedDatabase)
-}
-
-saveDatabase(listArray, "ArrayOfTasks")
-
-//____________LOAD THE ARRAY LIST FROM LOCAL STORAGE______________________________________
-const loadDatabase = function (localStorageKey) {
-    // Get the string version of the database
-    const databaseString = localStorage.getItem(localStorageKey)
-
-    // Use JSON.parse() to convert the string back into an object
-    return JSON.parse(databaseString)
-}
-
-// pull database back from local storage
-let allTheStuff = loadDatabase("ArrayOfTasks");
-
-console.log("This is the array list of tasks", allTheStuff);
-
-// function writeTaskToDom = () => {
-//       for (let i = 0; i < )
-//   }
-=======
->>>>>>> master
