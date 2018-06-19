d33c7a425d990b644f05d136ea4521b8d7c72a2e
Lauren Richert <lsrichert@gmail.com>
2018-06-19 09:59:57 -0500

    getting new master

--- src/main.js
+++
@@ -1,9 +0,0 @@
-const saveDatabase = function (databaseObject, localStorageKey) {
-
-    const stringifiedDatabase = JSON.stringify(databaseObject)
-
-    console.log("database object", databaseObject)
-    localStorage.setItem(localStorageKey, stringifiedDatabase)
-}
-
-saveDatabase(listArray, "ArrayOfTasks")
